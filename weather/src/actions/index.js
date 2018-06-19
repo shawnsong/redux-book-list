ed2e9fbd81f36378612d0e7d2e241b5b38cb60d2
Shawn Song <mr.song.shawn@gmail.com>
2018-06-19 20:13:55 +1000

    added redux-promise, axios

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,2 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'c07929925567aa2209dccf4a73ec0272';
 
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEHATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
+    return {
+        type: FETCH_WEHATHER,
+        payload: request
+    };
+}
