06d9feb43d5cae8fe98801fda18d7efd1d5357ed
Shawn Song <mr.song.shawn@gmail.com>
2018-06-26 21:32:19 +1000

    google maps

--- weather/src/containers/weather-list.js
+++ weather/src/containers/weather-list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google-map';
 
  class WeatherList extends Component {
     constructor(props) {
@@ -13,11 +14,12 @@ import Chart from '../components/chart';
         const temps = cityData.list.map(item => item.main.temp);
         const pressures = cityData.list.map(item => item.main.pressure);
         const humidities = cityData.list.map(item => item.main.humidity);
+        const {lon, lat} = cityData.city.coord;
 
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="red" /></td>
                 <td><Chart data={pressures} color="green" /></td>
                 <td><Chart data={humidities} color="orange" /></td>
